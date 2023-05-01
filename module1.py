163d97437a9042940436ed68bc554dc8c4a019e2
RENUKAMANORE <renukamanore10@gmail.com>
2023-05-01 08:22:05 +0530

    first commit

---
+++ module1.py
@@ -0,0 +1,12 @@
+from renu_module import starline ,hashline
+import renu_module,sys
+import os
+
+starline(50)
+hashline(20)
+print(type(renu_module))
+print(dir(renu_module))
+
+print(sys.modules)
+
+print(os.getcwd()) # current working directory
---
+++ renu_module.py
@@ -0,0 +1,10 @@
+def starline(n):
+    """ prints a line nth no of stars """
+    print("*"*  n)
+
+def hashline(n):
+    """ prints a with nth no of hash star"""
+    print("#" * n)
+
+def dashline(n):
+    print("-" * n)
